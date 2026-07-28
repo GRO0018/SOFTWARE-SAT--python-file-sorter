4e9adda48ffd8a5bfe39afab2a6dae750a2d37c6
GRO0018 <gro0018@bayside.vic.edu.au>
2026-07-29 09:25:35 +1000

    -added the def, sort_files. -connects to downloads -generates folder in downloads that has categories -dose a check if downloads folder exists

--- File-sorting-program.py
+++ File-sorting-program.py
@@ -1,1 +1,27 @@
 # I hate this potato of a laptop
+import os 
+import shutil
+
+#File sort
+def sort_files():
+    #Connects to downloads folder
+    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+
+    #Creates folder to sort into
+    base_folder = os.path.join(downloads_path, "School2026")
+
+    categories = {
+        "english": "English",
+        "math": "Math",
+        "software": "Software",
+        "outdoor": "Outdoor"
+    }
+
+    os.makedirs(base_folder, exist_ok=True)
+
+    for folder in categories.values():
+        os.makedirs(os.path.join(base_folder, folder), exist_ok=True)
+
+    #Downloads folder check
+
+sort_files()

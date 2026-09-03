84af9a50935ac47dd33b596e4a173cfb03efdfb9
GRO0018 <gro0018@bayside.vic.edu.au>
2026-09-03 11:25:21 +1000

    Push 2 -Started on GUI -Imported tkinter and then ttk -Setup the window -added frame -Title 'Classwork Sorting Program' -setup buttone to sort files

--- File-sorting-program.py
+++ File-sorting-program.py
@@ -1,12 +1,12 @@
-# I hate this potato of a laptop
-import os 
+import os
 import shutil
+import tkinter
+from tkinter import ttk
 
 #File sort
 def sort_files():
     #Connects to downloads folder
     downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-
     #Creates folder to sort into
     base_folder = os.path.join(downloads_path, "School2026")
 
@@ -23,5 +23,38 @@ def sort_files():
         os.makedirs(os.path.join(base_folder, folder), exist_ok=True)
 
     #Downloads folder check
+    moved_files = []
 
-sort_files()
+    for item in os.listdir(downloads_path):
+        source = os.path.join(downloads_path, item)
+
+        if os.path.isfile(source):
+            filename_lower = item.lower()
+
+            for keyword, folder_name in categories.items():
+                if keyword in filename_lower:
+                    destination = os.path.join(base_folder, folder_name, item)
+                    shutil.move(source, destination)
+                    moved_files.append(f"{item}  →  {folder_name}")
+                    break
+
+
+#GUI-----------------------------------------------------
+#window
+root = tkinter.Tk()
+root.title('Python File Sorter')
+root.geometry('500x400')
+#freame
+frm = ttk.Frame(root, padding=10)
+frm.pack(fill='both', expand=True)
+
+#Title
+title = ttk.Label(frm, text='Classwork Sorting Program')
+title.pack(pady=5)
+
+#sort button
+sort_button = ttk.Button(frm, text='Sort Files', command=sort_files)
+sort_button.pack(pady=10)
+
+
+root.mainloop()
